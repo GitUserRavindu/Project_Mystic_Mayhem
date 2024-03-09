3d631501f634de6af8c8c0d430da9a5df89e2f21
YutharsanS <yutharsan.22@cse.mrt.ac.lk>
2024-03-09 12:47:33 +0530

    Change the access modifiers and recfactor vairable names

--- Character/Character.java
+++ Character/Character.java
@@ -2,16 +2,16 @@ package Character;
 import Equipment.Equipment;
 
 public abstract class Character {
-    public String name;
-    public float health;
-    public float attack;
-    public float defense;
-    public float speed;
-    public String tribe;
-    public int prize;
-    public String type;
-    public Equipment[] equipment;
-    public Boolean status;
+    protected String name;
+    protected float health;
+    protected float attack;
+    protected float defense;
+    protected float speed;
+    protected String tribe;
+    protected int prize;
+    protected String type;
+    protected Equipment[] equipments;
+    protected Boolean status;
 
     // Constructors
 
@@ -35,62 +35,73 @@ public abstract class Character {
     }
 
     // getters and setters
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
     public float getHealth() {
         return health;
     }
     public void setHealth(float health) {
         this.health = health;
     }
+
     public float getAttack() {
         return attack;
     }
     public void setAttack(float attack) {
         this.attack = attack;
     }
+
     public float getDefense() {
         return defense;
     }
     public void setDefense(float defense) {
         this.defense = defense;
     }
+
     public float getSpeed() {
         return speed;
     }
     public void setSpeed(float speed) {
         this.speed = speed;
     }
+
     public String getTribe() {
         return tribe;
     }
+
     public void setTribe(String tribe) {
         this.tribe = tribe;
     }
+
     public int getPrize() {
         return prize;
     }
+
     public void setPrize(int prize) {
         this.prize = prize;
     }
+
     public String getType() {
         return type;
     }
+
     public void setType(String type) {
         this.type = type;
     }
+
     public Boolean getStatus() {
         return status;
     }
-
     public void setStatus(Boolean status) {
         this.status = status;
     }
 
-    // abstract classes
+    // abstract methods
     public abstract Character makeClone();
 }
