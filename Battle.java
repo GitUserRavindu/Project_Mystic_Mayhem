3afaee9313ae388150f7a41979a380beb26bed83
HimalaGunathilaka <157564894+HimalaGunathilaka@users.noreply.github.com>
2024-03-11 22:47:10 +0530

    Updated battle class - not finished

--- Battle.java
+++ Battle.java
@@ -1,55 +1,138 @@
 import java.util.ArrayList;
 
-public class Battle implements BattleFlow {
-    public static String ground;
-    public ArrayList<Character> armyBuffed;
-    @Override
-    public void setBonus(Player player, String ground) {
-        ground = player.getHomeGround();
-        switch (ground) {
+import Character.Character;
+public class Battle {
+    public ArrayList<Character> army1,army2;
+    public String home;
+
+    public Battle(String home,ArrayList<Character> army1,ArrayList<Character> army2) {
+        this.home = home;
+        this.army1= army1;
+        this.army2= army2;
+    }
+    public void Buff(ArrayList<Character> army){
+        switch(home){
             case "Hillcrest":
-                // armyBuffed = player.getArmy();
-                // for (Character character : armyBuffed) {
-                //     if (character.getTribe().equalsto("Highlander")) {
-                //         character.setAttackPower(character.getAttackPower() * 1.2);
-                //     }
-                // }
+                for(Character c:army){
+                    if(c.getTribe().equals("Highlander")){
+                        c.setAttack(c.getAttack()+1f);
+                        c.setDefense(c.getDefense()+1f);
+                    }else if(c.getTribe().equals("Marshlanders")){
+                        c.setAttack(c.getSpeed()-1f);}
+                }
                 break;
             case "Marshland":
-                    
+                for(Character c:army){
+                    if(c.getTribe().equals("Marshlander")){
+                        c.setDefense(c.getDefense()+2f);}
+                    else if(c.getTribe().equals("Highlander")){
+                        c.setAttack(c.getAttack()-1f);
+                    }else if(c.getTribe().equals("Mystic")){
+                        c.setSpeed(c.getSpeed()-1f);
+                    }  
+                }
                 break;
             case "Desert":
-
+                for(Character c:army){
+                    if(c.getTribe().equals("Marshlander")){
+                        c.setHealth(c.getHealth()-1f);
+                    }else if(c.getTribe().equals("Sunchild")){
+                        c.setAttack(c.getAttack()+1f);
+                    }
+                }
                 break;
             case "Arcane":
-                        
+                for(Character c:army){
+                    if(c.getTribe().equals("Mystic")){
+                        c.setAttack(c.getAttack()+2f);
+                    }else if(c.getTribe().equals("Highlander")){
+                        c.setSpeed(c.getSpeed()+1f);
+                    }
+                    else if(c.getTribe().equals("Marshlander")){
+                        c.setSpeed(c.getSpeed()-1f);
+                        c.setDefense(c.getDefense()-1f);
+                    }
+                }
                 break;
-        
             default:
                 break;
         }
     }
-
-    @Override
-    public void Battling(ArrayList<Character> army1, ArrayList<Character> army2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Battling'");
+    public void resetBuff(ArrayList<Character> army){
+        switch(home){
+            case "Hillcrest":
+                for(Character c:army){
+                    if(c.getTribe().equals("Highlander")){
+                        c.setAttack(c.getAttack()-1f);
+                        c.setDefense(c.getDefense()-1f);
+                    }else if(c.getTribe().equals("Marshlanders")){
+                        c.setAttack(c.getSpeed()+1f);}
+                }
+                break;
+            case "Marshland":
+                for(Character c:army){
+                    if(c.getTribe().equals("Marshlander")){
+                        c.setDefense(c.getDefense()-2f);}
+                    else if(c.getTribe().equals("Highlander")){
+                        c.setAttack(c.getAttack()+1f);
+                    }else if(c.getTribe().equals("Mystic")){
+                        c.setSpeed(c.getSpeed()+1f);
+                    }  
+                }
+                break;
+            case "Desert":
+                for(Character c:army){
+                    if(c.getTribe().equals("Marshlander")){
+                        //c.setHealth(c.getHealth()+1f);
+                    }else if(c.getTribe().equals("Sunchild")){
+                        c.setAttack(c.getAttack()-1f);
+                    }
+                }
+                break;
+            case "Arcane":
+                for(Character c:army){
+                    if(c.getTribe().equals("Mystic")){
+                        c.setAttack(c.getAttack()-2f);
+                    }else if(c.getTribe().equals("Highlander")){
+                        c.setSpeed(c.getSpeed()-1f);
+                    }
+                    else if(c.getTribe().equals("Marshlander")){
+                        c.setSpeed(c.getSpeed()+1f);
+                        c.setDefense(c.getDefense()+1f);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     }
-
-    @Override
-    public void resetHealth(ArrayList<Character> army) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resetHealth'");
+    public void resetHealth(ArrayList<Character> army){
+        for(Character c:army){
+            c.setHealth(c.getMaxHealth());
+        }
     }
-
-    @Override
-    public void reSetBonus() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'reSetBonus'");
+    public void fight(ArrayList<Character> army1,ArrayList<Character> army2,String name1,String name2){
+        Buff(army1);
+        Buff(army2);
+        int i=0;
+        System.out.println(name1+" vs "+name2);
+        Boolean cont=true,w1=false,w2=false;
+        while(cont){
+            i++;
+            for(Character c1:army1){
+                if(c1.getStatus()){cont=true;w2=true;break;}
+                else{cont=false;}
+            }
+            for(Character c1:army1){
+                if(c1.getStatus()){cont=true;w1=true;break;}
+                else{cont=false;}
+            }
+            for(int j=0;j<5;j++){
+                System.out.println("Turn "+i+":"+name1);
+                
+                System.out.println("Turn "+i+":"+name2);}
+        }
+        resetBuff(army1);
+        resetBuff(army2);
     }
-    
-    
-
-
-    
 }
