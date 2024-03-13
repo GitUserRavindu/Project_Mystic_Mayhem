3014d84bfac176779d81166a89c4eac1e42bc91b
GitUserRavindu <105047448+GitUserRavindu@users.noreply.github.com>
2024-03-13 16:57:27 +0530

    Cleaned the Player Class
    Need attention to setHomeGroundBonus method.

--- Player.java
+++ Player.java
@@ -1,108 +1,62 @@
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.Serializable;
 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 import Character.Character;
+
+import HomeGround.HomeGround;
 import HomeGround.Arcane;
 import HomeGround.Desert;
 import HomeGround.Hillcrest;
-import HomeGround.HomeGround;
 import HomeGround.Marshland;
 
 public class Player implements Serializable {
 
-    Scanner sc = new Scanner(System.in);
+    // Player attributes ------------------------------------------------------------------------------    
+    private String name;//Can be change after creation
+    private String userName;//Can not be change after creation. Unique for each user.
+    private int userId; // A unique id for each user. Can be generated using number of users.
+    private int goldCoins;
+    private float xp;
+    private HomeGround homeGround; //HomeGround is a class that contains the home ground of the player.
+    public ArrayList<Character> army = new ArrayList<Character>(); //////!!!!!Public, Check with market    
+    // End of Player Attributes -----------------------------------------------------------------------   
+    
+    
+    // Constructor ------------------------------------------------------------------------------------
+    public Player() {}
+    // End of Constructor -----------------------------------------------------------------------------
+    
+    
+    // Getters ----------------------------------------------------------------------------------------
+    public String getName() { return name;}
 
-    public String name;//Can be change after creation
-    public int gold;
-    public float xp;
-    public String UserName;//Can not be change after creation. Like a email.
-    public int UserId = 0; // A unique id for each user. Can be generated using number of users.
-    public ArrayList<Character> army = new ArrayList<Character>();
+    public String getUserName() { return userName; }
 
+    public int getUserId() { return userId; }
+    
+    public int getGold() { return goldCoins; }
+        
+    public float getXp() { return xp; }
+    
+    public HomeGround getHomeGround() { return homeGround; }
+  
+    public ArrayList<Character> getArmy() { return army; }
+    // End of Getters ---------------------------------------------------------------------------------
+    
+    
+    // Setters ----------------------------------------------------------------------------------------
+    public void setName(String name) { this.name = name; }
+    
+    public void setUserName(String userName) { this.userName = userName; }
+    
+    public void setUserId(int userId) { this.userId = userId; }
 
-    //HomeGround is a class that contains the home ground of the player.
-    //Will the Player can change the home ground after creation?
-    public HomeGround homeGround;
+    public void setGold(int goldCoins) { this.goldCoins = goldCoins; }
 
-    public HomeGround getHomeGround() {
-        return homeGround;
-    }
-
-    public Player() {
-        this.gold = 500;
-    }
-
-    //How the shop will work? Is it static?
-    //public static Shop shop = new Shop();
-
-    //List of all players. Can be used to check if a username is already taken or not.
-    //If the game is off, this must be saved to a file and loaded when the game starts.
-    public static ArrayList<String> players = new ArrayList<String>();
-
-    public Player(String name, /* byte[]*/ String UserName)/*throws IOException*/ {
-
-        UserId++;
-        gold = 500;
-        xp = 0;//Should we inclde a level system? If yes, we can use xp to calculate the level.
-        this.setName(name);
-
-        //Check if the username is already taken. While loop will be used to check if the username is already taken.
-        //Have to check if this loop is working or not.
-        Scanner sc=new Scanner(System.in);
-        // InputStreamReader in=new InputStreamReader(System.in);
-        // BufferedReader bf=new BufferedReader(in);
-        while(players.contains(UserName)) {
-            UserName="";
-            // Handle the case when the UserName is already taken
-            System.out.println("Username is already taken");
-            System.out.println("Please enter a new username");
-            System.out.print("Username: ");
-            try {
-                UserName=sc.nextLine();
-            } catch (Exception e) {
-            } finally {
-                sc.close();
-            }
-        }
-        players.add(UserName);
-        this.setUserName(UserName);
-
-        //Didn't add for equipments. Will be added later.
-
-    }
-
-
-
-    public void setUserName(byte[] userName2) {
-        this.UserName = new String(userName2);
-    }
-
-    public void addCharacter(Character character, int position) {
-        for(Character c:army){
-            if(c.getName().equals(character.getName())){
-                System.out.println("Character is already in the army");
-                return;
-            }else if(c.getType().equals(character.getType())){
-                System.out.println("Character of this type is already in the army");
-                System.out.println("Do you want to sell the character in the position? (Y/N)");
-                char check = sc.next().charAt(0);
-                if (check == 'Y' || check == 'y') {
-                    //army.sell(c);
-                    //A selling method will be added later.
-                    army.remove(c);
-                    army.add(character);
-                    return;
-                } else {
-                return;
-            }}}
-    this.army.add(character);}
-
-    public String getName() {
-        return name;
-    }
+    public void setXp(float xp) { this.xp = xp; }
 
     public void setHomeGround() {
         System.out.println("Choose a home ground");
@@ -112,13 +66,13 @@ public class Player implements Serializable {
         System.out.println("4. Arcane");
         System.out.println("Enter the number of the home ground: \n");
 
-        //Did not implement for invalid integer input.
-        int n=0;
+        int choice;
+
         while (true) {
             try {
                 Scanner scanner = new Scanner(System.in);
-                n = scanner.nextInt();
-                if(n < 1 || n > 4) {
+                choice = scanner.nextInt();
+                if(choice < 1 || choice > 4) {
                     throw new IOException();
                 }
                 break;
@@ -127,7 +81,7 @@ public class Player implements Serializable {
             }
         }
 
-        switch (n) {
+		switch (choice) {
             case 1:
                 homeGround = new Hillcrest();
                 break;
@@ -144,7 +98,7 @@ public class Player implements Serializable {
                 System.out.println("Invalid input. Please enter a valid number."); //This will never be executed. I guess
         }
     }
-
+    
     // public void setHomeGroundBonus() {
     //     //Set the bonus of the home ground.
     //     //Most of them just increase the stats of the characters.
@@ -152,61 +106,23 @@ public class Player implements Serializable {
     //     homeGround.setBonus(army,bonusTurns);
     // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    //Since army attribute is public, this method is not necessary. 
+    //In the market, the characters are added to the army directly.
+    public void setArmy(ArrayList<Character> army) { this.army = army; }   
+    // End of Setters ---------------------------------------------------------------------------------
 
-    public int getGold() {
-        return gold;
-    }
-
-    public void setGold(int gold) {
-        this.gold = gold;
-    }
-
-    public float getXp() {
-        return xp;
-    }
-
-    public void setXp(float xp) {
-        this.xp = xp;
-    }
-
-    public String getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(String userName) {
-        UserName = userName;
-        players.add(userName);
-    }
-
-    public ArrayList<Character> getArmy() {
-        return army;
-    }
-
-    public void setArmy(ArrayList<Character> army) {
-        this.army = army;
-    }
     
-    public int getUserId() {
-        return UserId;
-    }
-
-    public void setUserId(int userId) {
-        UserId = userId;
-    }
-
+    // Other Methods ----------------------------------------------------------------------------------
     public void displayCurrentStatus() {
         System.out.println("Here's your profile status :");
         System.out.println("------------------------------------");
         System.out.println("---------- Profile Status ----------");
-        System.out.format("%12s %3s %20s", "UserName", " : " , UserName + "\n");
-        System.out.format("%12s %3s %20s", "UserId", " : " , UserId + "\n");
+        System.out.format("%12s %3s %20s", "UserName", " : " , userName + "\n");
+        System.out.format("%12s %3s %20s", "UserId", " : " , userId + "\n");
         System.out.format("%12s %3s %20s", "Name", " : " , name + "\n");
-        System.out.format("%12s %3s %20s", "GoldCoins", " : " , gold + "\n");
+        System.out.format("%12s %3s %20s", "GoldCoins", " : " , goldCoins + "\n");
         System.out.format("%12s %3s %20s", "XP", " : " , xp + "\n");
-        System.out.format("%12s %3s %20s", "HomeGround", " : " , homeGround + "\n");
+        System.out.format("%12s %3s %20s", "HomeGround", " : " , homeGround.getName() + "\n");
         System.out.print("        Army  : ");
         for (Character character : army) {
             System.out.print(character.getName() + ", ");
@@ -215,4 +131,5 @@ public class Player implements Serializable {
         System.out.println("------ End of Profile Status -------");
         System.out.println("------------------------------------");
     }
+    // End of Other Methods ---------------------------------------------------------------------------
 }
