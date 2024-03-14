1e4397de0456ad3e54f6d1b7672f9fc74c2bb736
GitUserRavindu <105047448+GitUserRavindu@users.noreply.github.com>
2024-03-14 19:26:02 +0530

    buy recommended characters in profile creation
    market and profile updated

--- Profile.java
+++ Profile.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 import Utils.ScannerUtil;
 
+import Character.Character;
+
 public class Profile {
 
     // Profile attributes ----------------------------------------------------------------------------------------------
@@ -12,7 +14,7 @@ public class Profile {
     private static HashMap<Integer, Player> playerMap = new HashMap<Integer, Player>(); // map of userIds to profiles.should it be public?
     // End of Profile Attributes ---------------------------------------------------------------------------------------
 
-    
+
     // Profile Getters -------------------------------------------------------------------------------------------------
     public static int getProfileCount() { return profileCount; }
     public static ArrayList<String> getUserNameList() { return userNameList; } //This will not be used in the game.
@@ -87,7 +89,7 @@ public class Profile {
 
         // Player Homeground
         System.out.println("---> Please Enter a HomeGround : \n");
-        player.setHomeGround(); // Fix: Correct method call to set the player's home ground
+        player.setHomeGround();
         System.out.println("Your homeground has been set to : " + player.getHomeGround().getName() + "\n");
 
 
@@ -95,7 +97,52 @@ public class Profile {
         player.setHomeGroundName(player.getHomeGround().getName());
 
         // Player Army
-        System.out.println("---> For building your army, Please visit to the market menu in the main menu. \n");
+        System.out.println("---> Please build your army\n");
+        System.out.println("You have 500 gold coins to spend on your army.");
+        System.out.println("Your army should consist of at least one character from each of the following categories:");
+        System.out.println("    1. Archer");
+        System.out.println("    2. Knight");
+        System.out.println("    3. Mage");
+        System.out.println("    4. Healer");
+        System.out.println("    5. Mythical Creature\n");
+        System.out.println("Since this is your first time in building an army, we encourage you to buy");
+        System.out.println("the following 5 characters that represent each category.");
+        System.out.println("    1. Shooter  - 80 gold coins");
+        System.out.println("    2. Squire   - 85 gold coins");
+        System.out.println("    3. Warlock  - 100 gold coins");
+        System.out.println("    4. Soother  - 95 gold coins");
+        System.out.println("    5. Dragon   - 120 gold coins");
+        System.out.println("As you win more battles, you can earn more gold coins and buy more characters.\n");
+        System.out.println("Proceed to buy the recommended characters? (y/n)\n");
+
+        String proceed = ScannerUtil.scanner.nextLine();
+
+        while (proceed.length() == 0) {
+            proceed = ScannerUtil.scanner.nextLine();
+        }
+
+        if (proceed.equals("y")) {
+            Character character = Registry.returnCharacter("Shooter");
+            player.army.add(character);
+            character = Registry.returnCharacter("Squire");
+            player.army.add(character);
+            character = Registry.returnCharacter("Warlock");
+            player.army.add(character);
+            character = Registry.returnCharacter("Soother");
+            player.army.add(character);
+            character = Registry.returnCharacter("Dragon");
+            player.army.add(character);
+            player.setGold(player.getGold() - 480);
+
+            System.out.println("You have successfully bought the recommended characters.");
+            System.out.println("Your available gold coins : " + player.getGold() + "\n");
+        }
+        else {
+            System.out.println("You have chosen to skip buying the recommended characters.");
+            System.out.println("You will not be able to battle without a complete army."); 
+            System.out.println("No worries, you can buy characters from the market place later.\n");
+            System.out.println("Your available gold coins : " + player.getGold() + "\n");
+        }
 
         // Add to playerMap
         playerMap.put(player.getUserId(), player);
